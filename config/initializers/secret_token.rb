8a722c19f9e3cf22e007b925582b216dbe379cd1
Einar Jonsson <einar@transmit.is>
2013-03-29 13:32:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buzzcut::Application.config.secret_token = '9f8bb7437e68e7c3feaf4641159bae0c0642777ab09a6446aace4410922c90584144660ed8929697982cc818896ab9c661ea82b51b7567877fc15bbdcc033e84'
