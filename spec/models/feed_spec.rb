0dcc618a578314c3f2731567b178adcbb415dd82
Einar Jonsson <einar@codelette.com>
2013-06-09 22:33:26 +0000

    Added a placeholder spec for the Feed model

---
+++ spec/models/feed_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Feed do
+  it "Should encapsulate a feed" do
+    pending "Feed functionality is not yet implemented. It should represent a grouping mechanism, such as encapsulating a specific twitter list or search"
+  end
+end
